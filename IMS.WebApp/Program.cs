104fd579783c5037d048c8e48130a6d53c40d136
DibsTHEgreat <pateliyandivya@gmail.com>
2025-02-16 13:18:48 -0700

    adding product classes

--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -31,6 +31,8 @@ builder.Services.AddTransient<IDeleteInventoryUseCase, DeleteInventoryUseCase>()
 
 builder.Services.AddTransient<IViewProductsByNameUseCase, ViewProductsByNameUseCase>();
 builder.Services.AddTransient<IDeleteProductUseCase, DeleteProductUseCase>();
+builder.Services.AddTransient<IAddProductUseCase, AddProductUseCase>();
+builder.Services.AddTransient<IEditProductUseCase, EditProductUseCase>();
 
 var app = builder.Build();
 

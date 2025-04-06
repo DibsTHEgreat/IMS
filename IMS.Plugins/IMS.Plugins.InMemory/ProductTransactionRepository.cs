d45d9499359c9b3c448c1294e09b49fbf3c683f6
DibsTHEgreat <pateliyandivya@gmail.com>
2025-04-06 01:01:22 -0600

    progress

--- IMS.Plugins/IMS.Plugins.InMemory/ProductTransactionRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/ProductTransactionRepository.cs
@@ -66,5 +66,22 @@ namespace IMS.Plugins.InMemory
                 CreatedBy = createdBy,
             });
         }
+
+        public Task SellProductAsync(string salesOrderNumber, Product product, int quantity, string doneBy)
+        {
+            this._productTransactions.Add(new ProductTransaction
+                {
+                    ActivityType = ProductTransactionType.SellProduct,
+                    SONumber = salesOrderNumber,
+                    ProductId = product.ProductId,
+                    QuantityBefore = product.Quantity,
+                    QuantityAfter = product.Quantity - quantity,
+                    TransactionDate = DateTime.Now,
+                    CreatedBy = doneBy,
+                    UnitPrice = product.Price,
+                });
+
+            return Task.CompletedTask;
+        }
     }
 }
